e78d7ab4201ae099f883876c7f19246abacbea42
ceynri <347670115@qq.com>
2019-10-25 23:07:05 +0800

    update updated time

---
+++ rename.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成文章最后更新时间 ###################
+
+mv docs/notes/.git docs/notes/origin.git
+git add -A
+git commit -m 'update updated time'
+
+# rename docs/notes/origin.git docs/notes/git
